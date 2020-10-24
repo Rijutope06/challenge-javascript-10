3199985a47a8c31bc21a869406e41ed7b0864ca5
Junior <jupeto@hotmail.es>
2020-10-24 12:23:27 -0500

    challenge finished

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,35 @@
-const getMaxNumber = (numbersArray) => {
-  // your code here
+const faker = require("faker");
+
+const numbersArray = [];
+
+for (let index = 0; index < 1000; index++) {
+  numbersArray.push(faker.random.number(1000));
 }
+
+const getMaxNumber = (numbersArray) => {
+  return numbersArray.reduce((number, value) => {
+    if (number < value) {
+      number = value;
+    }
+    return number;
+  });
+};
 
 const getMinNumber = (numbersArray) => {
-  // your code here
-}
+  return numbersArray.reduce((number, value) => {
+    if (number > value) {
+      number = value
+    }
+    return number;
+  });
+};
 
 const getAverage = (numbersArray) => {
-  // your code here
-}
+  let total = numbersArray.reduce((number, value) => {
+    return number = value
+  });
+  let average = total / numbersArray.length;
+  return mean;
+};
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
